8ddcd8c9b5b80821ce88c749305891ca33e85e0c
ちはそ <cycyccq@gmail.com>
2020-02-25 15:12:34 +0900

    APIキーを保存するファイルを追加

--- lib/search.rb
+++ lib/search.rb
@@ -3,8 +3,8 @@
 require "pry"
 require "net/http"
 require "uri"
-require_relative "parse_text.rb"
-
+require_relative "parse_text"
+require_relative "../key"
 
 module MySearch
   class Search
@@ -14,6 +14,14 @@ module MySearch
 
     def unknown_words
       MySearch::ParseText.new(@input_text).extract_unknown_words
+    end
+
+    def urls
+      urls = []
+      unknown_words.each() { |word|
+        urls << "https://www.googleapis.com/customsearch/v1?&q=#{word}hl=ja&num=3&key=AIzaSyBFK2YRJNdxPS-4r2UY0bQi9D2vI1eXxzM&cx=015777728550736801855:mbplinrwlco"
+      }
+      urls
     end
 
     def search
